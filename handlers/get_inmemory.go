aab0c30d1d49e77b2c31c1d6a3550b0be25c6afe
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-04-30 21:12:52 +0100

    server and inmemory handler started

--- handlers/get_inmemory.go
+++ handlers/get_inmemory.go
@@ -1,5 +1,18 @@
 package handlers
 
+import "net/http"
+
+type RecsInMem struct {
+}
+
+func NewRecsInMem() *RecsInMem {
+	return &RecsInMem{}
+}
+
+func (inmem *RecsInMem) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Response from RecsInMem"))
+}
+
 // /GET from map
 
 // 3. GET request payload:
--- main.go
+++ main.go
@@ -1,17 +1,54 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/borowiak-m/interview-assignment-week-17/handlers"
+)
 
 func main() {
 	// mongodb+srv://challengeUser:WUMgIwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true
 
-	// server
+	// handlers
 	sm := http.NewServeMux()
-	http.ListenAndServe(":3000", sm)
+	// 	Records in memory handler
+	inmemHandler := handlers.NewRecsInMem()
 
-	// routes
+	// register routes
 	// /GET records from Mongo
-	// /GET from map
-	// /POST to map
+	// /GET from map / records in memory
+	sm.Handle("/inmemory", inmemHandler)
+	// /POST to map / records in memory
+
+	// define server
+	s := &http.Server{
+		Addr:         ":3000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	// non-blocking listen and serve
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// graceful shutdown
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	sig := <-sigChan
+	fmt.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 
 }
